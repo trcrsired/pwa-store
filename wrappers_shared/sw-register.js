b9589552afeaf892a259e6933076fb957f223531
trcrsired <uwgghhbcad@gmail.com>
2025-07-28 05:34:20 +0800

    shared

--- wrappers_shared/sw-register.js
+++ wrappers_shared/sw-register.js
@@ -1,6 +1,6 @@
 // shared/sw-register.js
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("../wrappers_shared/sw-register.js")
+  navigator.serviceWorker.register("./sw-register.js")
     .then((reg) => {
       const { cacheName } = window.appConfig;
       reg.active?.postMessage({ type: "INIT_CACHE", cacheName });
