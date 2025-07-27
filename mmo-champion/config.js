f2a74e58d54c00f8e6584eac690d2907c03551e9
trcrsired <uwgghhbcad@gmail.com>
2025-07-28 05:56:07 +0800

    add mmo-champion and warcraftlogs

---
+++ mmo-champion/config.js
@@ -0,0 +1,7 @@
+// config.js
+window.appConfig = {
+  title: "MMO Champion",
+  url: "https://www.mmo-champion.com",
+  cacheName: "pwa-mmochampion",
+  localStorageKey: "mmochampion_installed"
+};
---
+++ wacraftlogs/config.js
@@ -0,0 +1,7 @@
+// config.js
+window.appConfig = {
+  title: "Warcraft Logs",
+  url: "https://www.warcraftlogs.com/",
+  cacheName: "pwa-warcraftlogs",
+  localStorageKey: "warcraftlogs_installed"
+};
