fc3ca2f913657c775d46ba13e561f2b5dc530f84
trcrsired <uwgghhbcad@gmail.com>
2025-07-29 10:34:15 +0800

    fix mmo-champion

--- wrappers/mmo-champion/config.js
+++ wrappers/mmo-champion/config.js
@@ -1,6 +1,6 @@
 window.appConfig = {
   title: "MMO Champion",
   url: "https://www.mmo-champion.com",
-  cacheName: "pwa-mmochampion",
-  localStorageKey: "mmochampion_installed"
+  cacheName: "pwa-mmo-champion",
+  localStorageKey: "mmo-champion_installed"
 };
