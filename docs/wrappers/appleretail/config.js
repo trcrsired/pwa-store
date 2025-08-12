f9de235af8dae4b2bdff5347e9ca2dc156856a9b
trcrsired <uwgghhbcad@gmail.com>
2025-08-12 16:18:29 +0800

    add apple retail store and apple store

---
+++ docs/wrappers/appleretail/config.js
@@ -0,0 +1,6 @@
+window.appConfig = {
+  title: "Apple Retail Store",
+  url: "https://www.apple.com/retail",
+  cacheName: "pwa-appleretail",
+  localStorageKey: "appleretail_installed"
+};
---
+++ docs/wrappers/applestore/config.js
@@ -0,0 +1,6 @@
+window.appConfig = {
+  title: "Apple Store",
+  url: "https://www.apple.com/store",
+  cacheName: "pwa-applestore",
+  localStorageKey: "applestore_installed"
+};
