5fcd7406f9f615285cb4a2b1c48fb0ab4ddf99f9
trcrsired <uwgghhbcad@gmail.com>
2025-08-31 03:32:54 +0800

    fix qingtingfm.webp

--- docs/store/categories.js
+++ docs/store/categories.js
@@ -1074,7 +1074,7 @@ export const categories = [
       { name: "Lightnovel Kingdom (轻之国度)", nameKey: "lightnovel_name", descriptionKey: "lightnovel_desc", icon: "icons/wrappers/lightnovelkingdom.webp", url: "/wrappers/lightnovelkingdom", apptype: "wrapper" },
       { name: "WeChat Read (微信读书)", nameKey: "wechatread_name", descriptionKey: "wechatread_desc", icon: "icons/wrappers/wechatread.webp", url: "/wrappers/wechatread", apptype: "wrapper" },
       { name: "Qingting FM (蜻蜓FM)", nameKey: "qingtingfm_name", descriptionKey: "qingtingfm_desc", icon:
-        "icons/wrappers/qtfm.webp", apptype: "wrapper" },
+        "icons/wrappers/qingtingfm.webp", apptype: "wrapper" },
       { name: "TED", nameKey: "ted_name", descriptionKey: "ted_desc", icon: "icons/wrappers/ted.webp", url: "/wrappers/ted", apptype: "wrapper" },
       { name: "Netflix", nameKey: "netflix_name", descriptionKey: "netflix_desc", icon: "icons/wrappers/netflix.webp", url: "/wrappers/netflix", apptype: "wrapper" }
     ]
