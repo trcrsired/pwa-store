d3683f721d6c18e2f1fe0585182a743863215161
trcrsired <uwgghhbcad@gmail.com>
2026-07-27 01:32:33 +0800

    chase (sign in) url

--- scripts/generate_wrappers.py
+++ scripts/generate_wrappers.py
@@ -155,7 +155,7 @@ WRAPPERS = [
   { "name": "Kook", "url": "https://www.kookapp.cn/app/" },
   { "name": "Tieba", "url": "https://tieba.baidu.com" },
   { "name": "Chase", "url": "https://chase.com", "apptype": "msedge" },
-  { "name": "Chase (Sign in)", "url": "https://chase.com", "keyify_name": "chasesignin", "icon": "../msedge/chase.webp" },
+  { "name": "Chase (Sign in)", "url": "https://secure.chase.com", "keyify_name": "chasesignin", "icon": "../msedge/chase.webp" },
   { "name": "Termux", "url": "https://termux.dev" },
   { "name": "iHeart", "url": "https://www.iheart.com", "apptype": "msedge" },
   { "name": "Plyric", "url": "https://plyric.com" },
